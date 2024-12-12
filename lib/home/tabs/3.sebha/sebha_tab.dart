c23f202e2ffbd4c5da5190588fa97938b43809f3
LojainHamza <lojainehab17@gmail.com>
2024-12-12 16:32:07 +0200

    finished sebha tab ui and logic.

--- lib/home/tabs/3.sebha/sebha_tab.dart
+++ lib/home/tabs/3.sebha/sebha_tab.dart
@@ -1,10 +1,122 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class SebhaTab extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:islami_app/my_app_color.dart';
+
+class SebhaTab extends StatefulWidget {
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  String tasbeehWord = 'سبحان الله';
+  int counter = 1;
+  double rotationAngle = 0;
+
+  final List<String> tasbeehWords = [
+    'سبحان الله',
+    'الحمد لله',
+    'لا إله إلا الله',
+    'الله أكبر',
+    'لا حول ولا قوة إلا بالله',
+  ];
+
+  void onSebhaTap() {
+    setState(() {
+      counter++;
+
+      if (counter > 33) {
+        counter = 1;
+        int currentIndex = tasbeehWords.indexOf(tasbeehWord);
+        tasbeehWord = tasbeehWords[(currentIndex + 1) % tasbeehWords.length];
+      }
+
+      rotationAngle += pi / 12;
+    });
+  }
+
+  void resetSebha() {
+    setState(() {
+      tasbeehWord = 'سبحان الله';
+      counter = 1;
+      rotationAngle = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.transparent,
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Center(child: Image.asset('assets/images/islami_logo.png')),
+            const SizedBox(height: 20),
+            Image.asset('assets/images/tasbeeh.png'),
+            Expanded(
+              child: GestureDetector(
+                onTap: onSebhaTap,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Transform.rotate(
+                      angle: rotationAngle,
+                      child: SizedBox(
+                        height: 400,
+                        width: 400,
+                        child: Image.asset('assets/images/Sebha.png',
+                            fit: BoxFit.contain),
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 60),
+                        Text(
+                          tasbeehWord,
+                          style: const TextStyle(
+                            color: MyAppColor.whiteColor,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          '$counter',
+                          style: const TextStyle(
+                            color: MyAppColor.whiteColor,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          bottom: 5,
+          right: 5,
+          child: ElevatedButton(
+            onPressed: resetSebha,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: MyAppColor.goldColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            child: const Text(
+              'Reset',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
